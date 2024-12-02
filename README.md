65bf8fcea42d39b262b8f3c5b5adeddacfa59d29
Luiz A. Amelotti <luiz@amelotti.com>
2024-12-02 15:19:47 -0300

    Versão 0.1 do plugin oh-my-zsh aws-ssm-vault

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # zsh-aws-ssm-vault
 Plugin para oh-my-zsh para interagir, via shell, com o AWS SSM Parameter Store
+
+
+## Requisitos
+Este plugin depende do AWS CLI instalado e devidamente configurado (https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) e do pacote jq. 
+Deve haver uma variável de ambiente ZSH_AWS_PROFILE definida como profile padrão do plugin
+
+
+## Uso
+Função get_ssm_parameter()
+get_ssm_parameter [-v] nome_do_parametro [--profile profile_name]
+- -v: verbose
+- nome_do_parametro: nome do parâmetro do SSM Paremeter Store
+- --profile: nome do profile caso use um AWS CLI profile diferente do padrão, definido em ZSH_AWS_PROFILE
+
+Função set_ssm_parameter()
+set_ssm_parameter [-v] --name "/nome/chave/configuracao" --value "valor" [--type String|SecureString] [--profile profile_name]
+- -v: verbose
+- --name: nome do parâmetro dentro do SSM Parameter Store
+- --value: valor do parâmetro
+- --type: se o tipo é String (default) ou SecureString (criptografado)
+- --profile: nome do profile caso use um AWS CLI profile diferente do padrão, definido em ZSH_AWS_PROFILE
+
+Função list_ssm_parameter()
+list_ssm_parameters [-v] [--profile profile_name] [--with-values]
+- -v: verbose
+- --profile: nome do profile caso use um AWS CLI profile diferente do padrão, definido em ZSH_AWS_PROFILE
+- --with_values: retorna a lista de parâmetros com seus valores
+
+## Autor
+Luiz A. Amelotti <luiz@amelotti.com>
---
+++ aws-ssm-vault.plugin.zsh
@@ -0,0 +1,244 @@
+#
+# Função para buscar o valor de um parâmetro no AWS Systems Manager Parameter Store
+# get_ssm_parameter path_do_parametro [profile_aws]
+#
+get_ssm_parameter() {
+    local verbose=0
+    local parameter_name=""
+    local aws_profile=""
+
+    # Verifica os argumentos
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -v|--verbose)
+                verbose=1
+                shift
+                ;;
+            --profile)
+                aws_profile="$2"
+                shift 2
+                ;;
+            *)
+                parameter_name="$1"
+                shift
+                ;;
+        esac
+    done
+
+    # Validação do nome do parâmetro
+    if [[ -z "$parameter_name" ]]; then
+        echo "Erro: Nome do parâmetro não fornecido."
+        return 1
+    fi
+
+    # Define o perfil AWS padrão, se não especificado
+    if [[ -z "$aws_profile" ]]; then
+        aws_profile=$ZSH_AWS_PROFILE
+    fi
+
+    # Busca o parâmetro usando AWS CLI
+    local parameter_value
+    parameter_value=$(aws --profile "$aws_profile" ssm get-parameter --name "$parameter_name" --with-decryption --query "Parameter.Value" --output text 2>/dev/null)
+
+    # Verifica se a busca foi bem-sucedida
+    if [[ $? -ne 0 ]]; then
+        if [[ $verbose -eq 1 ]]; then
+            echo "Erro: Não foi possível buscar o parâmetro '$parameter_name'. Verifique se ele existe e se as permissões estão corretas."
+        fi
+        return 2
+    fi
+
+    # Retorna o valor do parâmetro (com ou sem comentários)
+    if [[ $verbose -eq 1 ]]; then
+        echo "Parâmetro solicitado: $parameter_name"
+        echo "Valor do parâmetro: $parameter_value"
+    else
+        echo "$parameter_value"
+    fi
+
+    return 0
+}
+
+
+#
+# Função definir o valor de um parâmetro no AWS Systems Manager Parameter Store
+#
+set_ssm_parameter() {
+    local verbose=0
+    local parameter_name=""
+    local parameter_value=""
+    local parameter_type="String"  # Tipo padrão é "String"
+    local aws_profile=""
+
+    # Verifica os argumentos
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -v|--verbose)
+                verbose=1
+                shift
+                ;;
+            --profile)
+                aws_profile="$2"
+                shift 2
+                ;;
+            --type)
+                parameter_type="$2"
+                shift 2
+                ;;
+            --name)
+                parameter_name="$2"
+                shift 2
+                ;;
+            --value)
+                parameter_value="$2"
+                shift 2
+                ;;
+            *)
+                echo "Erro: Argumento inválido '$1'."
+                return 1
+                ;;
+        esac
+    done
+
+    # Validação de argumentos obrigatórios
+    if [[ -z "$parameter_name" || -z "$parameter_value" ]]; then
+        echo "Erro: Nome (--name) e valor (--value) do parâmetro são obrigatórios."
+        return 1
+    fi
+
+    # Define o perfil AWS padrão, se não especificado
+    if [[ -z "$aws_profile" ]]; then
+        aws_profile=$ZSH_AWS_PROFILE
+    fi
+
+    # Define ou atualiza o parâmetro usando AWS CLI
+    if [[ $verbose -eq 1 ]]; then
+        echo "Definindo o parâmetro..."
+        echo "Nome: $parameter_name"
+        echo "Valor: $parameter_value"
+        echo "Tipo: $parameter_type"
+        echo "Profile AWS: $aws_profile"
+    fi
+
+    aws --profile "$aws_profile" ssm put-parameter \
+        --name "$parameter_name" \
+        --value "$parameter_value" \
+        --type "$parameter_type" \
+        --overwrite \
+        >/dev/null 2>&1
+
+    if [[ $? -ne 0 ]]; then
+        if [[ $verbose -eq 1 ]]; then
+            echo "Erro: Falha ao definir ou atualizar o parâmetro '$parameter_name'."
+        fi
+        return 2
+    fi
+
+    if [[ $verbose -eq 1 ]]; then
+        echo "Parâmetro '$parameter_name' definido com sucesso!"
+    else
+        echo "Parâmetro definido com sucesso!"
+    fi
+
+    return 0
+}
+
+
+#
+# Função listar os valores dos parâmetros no AWS Systems Manager Parameter Store
+#
+list_ssm_parameters() {
+    local verbose=0
+    local include_values=0
+    local aws_profile=""
+    local next_token=""
+
+    # Verifica os argumentos
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            -v|--verbose)
+                verbose=1
+                shift
+                ;;
+            --profile)
+                aws_profile="$2"
+                shift 2
+                ;;
+            --with-values)
+                include_values=1
+                shift
+                ;;
+            *)
+                echo "Erro: Argumento inválido '$1'."
+                return 1
+                ;;
+        esac
+    done
+
+    # Define o perfil AWS padrão, se não especificado
+    if [[ -z "$aws_profile" ]]; then
+        aws_profile=$ZSH_AWS_PROFILE
+    fi
+
+    if [[ $verbose -eq 1 ]]; then
+        echo "Listando parâmetros no Parameter Store..."
+        echo "Profile AWS: $aws_profile"
+        echo "Incluir valores: $include_values"
+    fi
+
+    # Lista os parâmetros (paginação caso haja muitos)
+    while :; do
+        local response
+        if [[ -n "$next_token" ]]; then
+            response=$(aws --profile "$aws_profile" ssm describe-parameters --next-token "$next_token" --query "{Parameters: Parameters, NextToken: NextToken}" --output json 2>/dev/null)
+        else
+            response=$(aws --profile "$aws_profile" ssm describe-parameters --query "{Parameters: Parameters, NextToken: NextToken}" --output json 2>/dev/null)
+        fi
+
+        # Verifica se a chamada foi bem-sucedida
+        if [[ $? -ne 0 ]]; then
+            if [[ $verbose -eq 1 ]]; then
+                echo "Erro: Não foi possível listar os parâmetros. Verifique as permissões e o perfil AWS."
+            fi
+            return 2
+        fi
+
+        # Extrai os parâmetros e o próximo token
+        local parameters
+        parameters=$(echo "$response" | jq -r '.Parameters[] | .Name')
+        next_token=$(echo "$response" | jq -r '.NextToken // empty')
+
+        # Exibe os parâmetros
+        for param_name in $parameters; do
+            if [[ $include_values -eq 1 ]]; then
+                # Obtém o valor do parâmetro
+                local param_value
+                param_value=$(aws --profile "$aws_profile" ssm get-parameter --name "$param_name" --with-decryption --query "Parameter.Value" --output text 2>/dev/null)
+
+                # Verifica se a busca foi bem-sucedida
+                if [[ $? -ne 0 ]]; then
+                    echo "Erro ao obter o valor para '$param_name'."
+                    continue
+                fi
+
+                # Exibe a chave e o valor
+                echo "$param_name: $param_value"
+            else
+                # Exibe apenas a chave
+                echo "$param_name"
+            fi
+        done
+
+        # Sai do loop se não houver mais páginas
+        if [[ -z "$next_token" ]]; then
+            break
+        fi
+    done
+
+    if [[ $verbose -eq 1 ]]; then
+        echo "Listagem concluída."
+    fi
+
+    return 0
+}
+
